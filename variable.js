2ae9b91d91a4c01584defd3ef2151303e8ef69b4
muhammad-2002 <masumbilla10104@gmail.com>
2024-01-25 20:45:21 +0600

    number

--- variable.js
+++ variable.js
@@ -25,3 +25,16 @@ console.log(typeof name);
 // use camale case
 // use snake case 
 // use Pascle case
+
+// Numbers
+var apple = 4.4;//use parseint to inteser or parse flot
+var Orange = '40';//string
+var lichi = 'nff';//string
+var malta = 40;
+console.log(Orange +malta)//not a number
+
+
+var num1 =0.1;
+var num2 =0.3;
+var total =num1 +num2;
+console.log(total.toFixed(2))//2 fixed mean how much disite number 
