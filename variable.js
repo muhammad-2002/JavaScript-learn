53c55b12c4d3ed18e5664f1eaea795bf607460d7
muhammad-2002 <masumbilla10104@gmail.com>
2024-01-25 20:27:02 +0600

    naming variable

--- variable.js
+++ variable.js
@@ -1,3 +1,27 @@
+// milestone -4
 //variable Difine 5 things
   var mony = 500;
   console.log(mony)
+
+
+//   type of variable
+// numaric variable
+var mony = 600;
+
+// Bulian variable
+var result = true;
+// string
+var name ="Masum";
+// console.log
+console.log(typeof name);
+
+
+// variable naming 
+
+// don't use space
+// don't use javascript keyword
+// don't use small leater
+
+// use camale case
+// use snake case 
+// use Pascle case
