01de8ec00386546a3222e9207a6043d64e311728
muhammad-2002 <masumbilla10104@gmail.com>
2024-01-27 10:23:16 +0600

    condition

---
+++ condition.js
@@ -0,0 +1,16 @@
+// const ispased = false;
+// if(!ispased){
+//     console.log('biya dimu')
+// }
+// else {
+//     console.log("fun")
+// }
+
+let score = 75;
+let grade;
+if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+}
+console.log(grade);
---
+++ practictask.js
@@ -0,0 +1,17 @@
+/***
+
+Free Drinks
+
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk 
+
+***/
+
+let Burger =501;
+if(Burger>500){
+    console.log('coke is free')
+}
+else{
+    console.log('Chock price is 30tk')
+}
+
